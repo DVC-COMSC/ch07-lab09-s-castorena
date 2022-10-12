f80c29530b1a3813330321110fb433df21b2a89e
sarah castorena <ceanz@yahoo.com>
2022-10-12 15:25:00 -0700

    new commit

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 # ******************************
 # Make your Code
 # ******************************
+# Sarah Castorena
+
+names = []
+name = input('Enter names: ')
+names = name.split()
+sortednames = sorted(names)
+print(min(sortednames,key=len), max(sortednames,key=len))
